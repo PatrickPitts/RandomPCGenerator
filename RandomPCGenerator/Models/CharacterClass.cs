ea873360fd1c5602447e3fb3505f8b94cf4355af
PatrickPitts <patrick.pittsj@gmail.com>
2020-02-07 16:18:59 -0800

    Implementing CharacterClasses

---
+++ RandomPCGenerator/Models/CharacterClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomPCGenerator.Models
+{
+    public class CharacterClass
+    {
+        public int HitDie { set; get; }
+        public string[] SavingThrows { set; get; } = new string[2];
+        public string SubclassName { set; get; }
+        public string ClassName { set; get; }
+        public int NumSpellsKnown { set; get; }
+        public IList<int> NumSpellSlots { set; get; }
+        public IList<string> Spells { set; get; }
+        public string SpellModifier { set; get; }
+        public string Source { set; get; } = "PHB";
+
+        public CharacterClass() { }
+    }
+}
---
+++ RandomPCGenerator/Processors/CharacterClassProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RandomPCGenerator.Models;
+
+
+namespace RandomPCGenerator.Processors
+{
+    public class CharacterClassProcessor
+    {
+        public static CharacterClass getRandomCharacterClass()
+        {
+            CharacterClass character = new CharacterClass();
+        }
+    }
+}
